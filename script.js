4bba127896a7ea302c5fa3e8122207371af791cf
Metahumo <metahumo@outlook.com>
2025-03-13 22:11:11 +0100

    web-ciberseguridad-spn

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js - Funcionalidad básica
+
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeMessage = document.querySelector('header p');
+    welcomeMessage.innerHTML += " ¡Bienvenido a tu espacio de aprendizaje!";
+});
