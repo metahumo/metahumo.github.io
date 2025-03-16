0b4cd8525376c5ceaaed30c262691978193b7faf
Metahumo <metahumo@outlook.com>
2025-03-16 16:03:17 +0100

    Create script.js

---
+++ ejercicios/script.js
@@ -0,0 +1,6 @@
+/* script.js - Funcionalidad básica para la sección de Ejercicios Resueltos */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const headerParagraph = document.querySelector('header p');
+    headerParagraph.innerHTML += " | Explora el contenido de HTB - Nibbleblog";
+});
